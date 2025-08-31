d72959256b4b97e924020309b5696968418fea85
s.saramud <s.saramud@gmail.com>
2025-08-31 21:09:43 +0300

    schema mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { PrismaClient } from "@/generated/prisma/client";
 
-export default function Home() {
+const client = new PrismaClient()
+
+export default async function Home() {
+  const courses = await client.course.findMany()
+
+  console.log(courses)
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
